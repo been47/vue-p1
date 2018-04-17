bf6dd7eb125fb28b3eca184b7c7aea638f1e8eae
박규호 <gh954023@gmail.com>
2018-04-17 18:27:04 +0900

    [MOD] quill editor 변경

--- src/components/Content/Quill.vue
+++ src/components/Content/Quill.vue
@@ -1,57 +1,41 @@
 <template>
   <div class="content__box">
-    <quill-editor ref="myQuillEditor"
-                  :options="editorOption"
-                  @blur="onEditorBlur($event)"
-                  @focus="onEditorFocus($event)"
-                  @ready="onEditorReady($event)">
-    </quill-editor>
+    <p>ㅁㅁㅁ</p>
+    <div id="app">
+      <vue-editor v-model="content" :editorToolbar="customToolbar"></vue-editor>
+    </div>
   </div>
 </template>
 
 <script>
 // require styles
-import 'quill/dist/quill.snow.css'
-
-import {quillEditor} from 'vue-quill-editor'
+import {VueEditor} from 'vue2-quill-editor'
 
 export default {
   name: 'Quill',
   data () {
     return {
-      editorOption: {
-        // some quill options
-      }
+      content: '<h1>Html For Editor</h1>',
+      customToolbar: [
+        ['bold', 'italic', 'underline'],
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+        ['image', 'code-block']
+      ]
     }
   },
   components: {
-    quillEditor
+    VueEditor
   },
   methods: {
-    onEditorBlur (quill) {
-      console.log('editor blur!', quill)
-    },
-    onEditorFocus (quill) {
-      console.log('editor focus!', quill)
-    },
-    onEditorReady (quill) {
-      console.log('editor ready!', quill)
-    },
     setMainMenu () {
       const routeName = this.$router.currentRoute.path.split('/')[1]
       this.$root.$emit('mainMenu', routeName)
     }
   },
   computed: {
-    editor () {
-      return this.$refs.myQuillEditor.quill
-    }
   },
   created () {
     this.setMainMenu()
-  },
-  mounted () {
-    console.log('this is current quill instance object', this.editor)
   }
 }
 </script>
